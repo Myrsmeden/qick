567eaea5e459a7910f92882ea6f193f9c73b3302
Johan Myrsmeden <johan@myrsmeden.se>
2018-05-23 21:09:39 +0200

    Updating some dependencies and adding authentication

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var express = require('express');
-var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 require('dotenv').config()
 
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(process.env.MONGODB_URI);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'))
+db.once('open', function() {
+  console.log("Connected!")
+})
 
 var app = express();
 
@@ -12,38 +24,53 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+// Configuring Passport
+var passport = require('passport')
+var expressSession = require('express-session')
+app.use(expressSession({secret: process.env.secret}))
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.get('/', routes.index);
-app.get('/queue/:track/:name', routes.queue);
+// Using the flash middleware provided by connect-flash to store messages in session
+// and displaying in templates
+var flash = require('connect-flash');
+app.use(flash());
+ 
+// Initialize Passport
+var initPassport = require('./passport/init');
+initPassport(passport);
 
-// Makes connection asynchronously.  Mongoose will queue up database
-// operations and release them when the connection is complete.
-mongoose.connect(process.env.MONGODB_URI, function (err, res) {
-  if (err) {
-      console.log('ERROR connecting to: ' + process.env.MONGODB_URI + '. ' + err);
-  } else {
-      console.log('Succeeded connected to: ' + process.env.MONGODB_URI);
-  }
+// Set up routes
+var routes = require('./routes/index')(passport);
+app.use('/', routes);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection error:'))
-db.once('open', function() {
-  console.log("Connected!")
-})
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+          message: err.message,
+          error: err
+      });
+  });
+}
+
 
 var serve = http.createServer(app);
 var io = require('socket.io')(serve);
---
+++ routes/index.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var router = express.Router();
+var Queue = require('../models/queue');
+var QueueItem = require('../models/queueItem');
+
+var isAuthenticated = function (req, res, next) {
+	// if user is authenticated in the session, call the next() to call the next request handler 
+	// Passport adds this method to request object. A middleware is allowed to add properties to
+	// request and response objects
+	if (req.isAuthenticated())
+		return next();
+	// if the user is not authenticated then redirect him to the login page
+	res.redirect('/');
+}
+
+module.exports = function(passport){
+
+    router.get('/', function(req, res) {
+        Queue.find({}, function(err, queue) {
+            if ( err ) {
+                console.err(err);
+            }
+            res.render('index', { title: 'Muu', queue: queue });
+        })
+    })
+
+    router.get('/queue/:track/:name', function(req, res) {
+        var track = req.params.track;
+        var name = req.params.name;
+        Queue.findOne({name: track}, function(err, queue) {
+            var items = queue.items || []
+            res.render('queue', { title: 'Kön', track: track, name: name, queue: items });
+        })
+    })
+
+	/* GET login page. */
+	router.get('/login', function(req, res) {
+    	// Display the Login page with any flash message, if any
+		res.render('login', { message: req.flash('message') });
+	});
+
+	/* Handle Login POST */
+	router.post('/login', passport.authenticate('login', {
+		successRedirect: '/home',
+		failureRedirect: '/',
+		failureFlash : true  
+	}));
+
+	/* GET Registration Page */
+	router.get('/signup', function(req, res){
+		res.render('register',{message: req.flash('message')});
+	});
+
+	/* Handle Registration POST */
+	router.post('/signup', passport.authenticate('signup', {
+		successRedirect: '/admin',
+		failureRedirect: '/signup',
+		failureFlash : true  
+	}));
+
+	/* GET Home Page */
+	router.get('/admin', isAuthenticated, function(req, res){
+		res.render('admin', { user: req.user });
+	});
+
+	/* Handle Logout */
+	router.get('/signout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
+
+	return router;
+}
+
+
+
+
